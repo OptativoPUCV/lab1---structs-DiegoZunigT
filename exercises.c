37a7a0c497476a47ec82b93b7e6a7b0682c45ecb
Your Name <you@example.com>
2024-03-15 03:07:32 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -90,7 +90,7 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
 int checkSorted(int arr[], int size) 
 {
   int check;
-  printf("- - - %d - - -", size);
+  printf("%d", size);
   if(arr[0] <= arr[1]) check = 1;
   else if(arr[0] >= arr[1]) check = -1;
   int aux = check;
