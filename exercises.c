db885cefa6e82727797cb8402f0a263292039d37
Your Name <you@example.com>
2024-03-15 03:21:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -102,7 +102,7 @@ int checkSorted(int arr[], int size)
     aux++;
     if(i == (size - 1)) return 1;
   }
-  
+  printf("%d - %d\n", check, aux);
   aux = check;
   for(int i = 0; i < (size - 1); i++) {
     if(arr[i] < arr[i + 1]) aux = 1;
